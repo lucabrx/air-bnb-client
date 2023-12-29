206d5247777f1b249b4d9e583fefc3806431ef5c
Luka <lukabrkovic@skiff.com>
2023-12-29 20:13:52 +0100

    [fix] updated price button

--- src/pages/listings/price-section.tsx
+++ src/pages/listings/price-section.tsx
@@ -30,21 +30,8 @@ export function PriceSection({
 }: PriceSectionProps) {
   return (
     <div className="relative mt-2 flex items-center justify-between py-2">
-      {session?.id === listing?.ownerId && (
-        <div className="absolute -right-9 top-0">
-          <Button onClick={() => setOpenUpdatePrice(!openUpdatePrice)} className=" h-7 w-7 p-0" variant="ghost">
-            <Icons.Pen className="h-4 w-4" />
-          </Button>
-          {openUpdatePrice && listing?.price !== priceUpdate && (
-            <Button onClick={handleUpdate} className="h-7 w-7 p-0">
-              <Icons.Check className="h-4 w-4" />
-            </Button>
-          )}
-        </div>
-      )}
-
       {openUpdatePrice ? (
-        <div className="flex w-fit items-center justify-center gap-2">
+        <div className="relative flex w-fit items-center justify-center gap-2">
           <span className="flex w-[4.3rem] font-medium">Price: $</span>
           <Input
             className="-ml-4"
@@ -55,9 +42,23 @@ export function PriceSection({
         </div>
       ) : (
         <>
-          <h2>
-            Price: <span className="font-medium">${listing?.price}</span>
-          </h2>
+          <div className="relative">
+            {session?.id === listing?.ownerId && (
+              <div className="absolute -right-8 -top-0.5">
+                <Button onClick={() => setOpenUpdatePrice(!openUpdatePrice)} className=" h-7 w-7 p-0" variant="ghost">
+                  <Icons.Pen className="h-4 w-4" />
+                </Button>
+                {openUpdatePrice && listing?.price !== priceUpdate && (
+                  <Button onClick={handleUpdate} className="h-7 w-7 p-0">
+                    <Icons.Check className="h-4 w-4" />
+                  </Button>
+                )}
+              </div>
+            )}
+            <h2>
+              Price: <span className="font-medium">${listing?.price}</span>
+            </h2>
+          </div>
           <h2>
             Total:{" "}
             <span className="font-medium">
